c3265f1a51f51450e620f094474b8de6029c3787
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-03-16 21:28:15 -0300

    initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+//Cada uma destas funções funcionam como se fossem uma classe
+
+export const Container = styled.div`
+
+`;
+
+export const Header = styled.div`
+
+`;
