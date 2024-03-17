c7f4785296ef11031ecc7a5a03a7f01bb680880a
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-03-17 18:20:02 -0300

    Add imports and styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 
+
+
+
 //Cada uma destas funções funcionam como se fossem uma classe
 
 export const Container = styled.div`
@@ -8,5 +11,22 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.div`
+       background-color: darkblue;
+       height: 150px;
+       text-align: center;
+`;
 
+export const HeaderText = styled.div`
+       margin: 0;
+       padding: 0;
+       color: #fff;//tenho qua aprender a colocar a cor em uma variável
+       padding-top: 30px;//Para afastar o título da borda de cima
+`;
+
+
+export const Body = styled.div`
+
+       margin: auto;
+       max-width: 980px;
+       margin-bottom: 30px;//Para dar um respiro logo a baixo
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 
+//Importando os estilos
 import * as C from './App.styles';
 
+//Importando os tipos (types)
+import {Category} from './types/Category';
+import {Item} from './types/Item';
+
+//Importando os dados
+import {categories} from './data/categories';
+import {items} from './data/items';
+
+//Para manipular os dados (useState)
+import {useState} from 'react';
+
+//Importando funções
+import {getCurrentMonth} from './helpers/dateFilter';
+
 const App = () => {
+
+  const [list,setList] = useState(items);
+  //Aqui ainda poderiamos adicionar o type
+  //const [list,setList] = useState<Item[]>(items);
+  const [currentMonth,setCurrentMonth] = useState(getCurrentMonth());
+
   return (
     <C.Container>{/* C.AlgumaCoisa é usado como se fosse uma classe. É uma classe que está sendo configurada em App.styles*/}
       <C.Header>{/* É uma classe que está sendo configurada em App.styles*/}
-
+        <C.HeaderText>Sistema financeiro</C.HeaderText>
+        
       </C.Header>
+      <C.Body>
+          
+          {/*Área de informações */}
+
+          {/*Área de inserções */}
+          
+          {/*Tabela de itens*/}
+      </C.Body>
     </C.Container>
   );
 }
